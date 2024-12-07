117f669662a13918d1db71a62c11452f27103701
Lia Pires <liasth.pires@gmail.com>
2024-12-07 16:07:01 -0300

    feat: Implement Twilio for messaging

--- api/v1/routes.py
+++ api/v1/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 
-from client_mood_track_api.utils import CommentProcessor, TwilioHandler
+from client_mood_track_api.utils import CommentProcessor
+from client_mood_track_api.twilio import TwilioHandler
 
 v1_blueprint = Blueprint("v1", __name__)
 
